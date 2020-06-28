e50649fc48a057008aaaeca050dd77e2dcdcb7f8
lollykrown <joe_kayu@yahoo.com>
2020-06-28 23:36:54 +0100

    .

---
+++ models/Users.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, Datatypes) => {
+  return sequelize.define('user', {
+      id: {
+        type: Datatypes.UUIDV4,
+        primaryKey: true
+      },
+      first_name: {
+        type: Datatypes.STRING,
+        isAlphanumeric: true,
+        required: true,
+        allowNull: true
+      },
+      last_name: {
+        type: Datatypes.STRING,
+        required: true,
+        allowNull: true
+      },
+      username: {
+        type: Datatypes.STRING,
+        required: true,
+        allowNull: true,
+        len: [8, 20]
+      },
+      password: {
+        type: Datatypes.STRING,
+        required: true,
+        allowNull: true,
+        len: [8, 20]
+      },
+      email: {
+        type: Datatypes.STRING,
+        required: true,
+        allowNull: true,
+        len: [7, 100],
+        isEmail: true
+      },
+      updated_at: { type: Datatypes.DATETIME },
+      deleted_at: { type: Datatypes.DATETIME }
+  },{
+    underscore: true,
+    paranoid: true
+  })
+}
