0f43ceaaa66974a74135e3f281935f9f09ed549e
lollykrown <joe_kayu@yahoo.com>
2020-06-29 22:24:33 +0100

    .

--- models/index.js
+++ models/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -9,7 +7,8 @@ const debug = require('debug')('app:model-index')
 
 const db = {}
 
-debug(config)
+if (config.node_env === 'dev') debug(config);
+
 const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
   host: config.db.host,
   dialect: 'mysql',
@@ -42,12 +41,60 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-//Associations
+// ASSOCIATIONS
 
-//Permissions
-db.permission.hasMany(db.user)
+/** **********---------------*********
+ ************| Content SERVICE |*********
+ ************---------------********* */
+// Answer
+db.answer.belongsTo(db.question);
 
-//Users
+// Questions
+db.question.hasMany(db.answer);
+db.question.hasMany(db.response);
+db.question.hasMany(db.question_video);
+
+// Question Videos
+db.question_video.belongsTo(db.question);
+db.question_video.belongsTo(db.video);
+
+// Response
+db.response.belongsTo(db.question);
+
+// Videos
+db.video.hasMany(db.question_video);
+/** ***********-----------------*******
+ ************| COURSE SERVICE |*********
+ ************-----------------******* */
+// Courses
+db.course.hasMany(db.course_history);
+db.course.hasMany(db.course_section);
+
+// Course History
+db.course_history.belongsTo(db.course);
+
+// Course Sections
+db.course_section.belongsTo(db.course);
+db.course_section.belongsTo(db.section);
+
+// Section
+db.section.hasMany(db.course_section);
+db.section.hasMany(db.section_question);
+db.section.hasMany(db.section_video);
+
+// Section Question
+db.section_question.belongsTo(db.section);
+
+// Section Video
+db.section_video.belongsTo(db.section);
+
+/** **********---------------*********
+************| USER SERVICE |*********
+************---------------********* */
+// Permissions
+db.permission.hasMany(db.user);
+
+// Users
 db.user.belongsTo(db.permission);
 
 
