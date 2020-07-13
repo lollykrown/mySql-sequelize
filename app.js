77077d2feb893909391e97784d1898d5f838a519
lollykrown <joe_kayu@yahoo.com>
2020-07-14 00:09:00 +0100

    .

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
     next(createError(404));
